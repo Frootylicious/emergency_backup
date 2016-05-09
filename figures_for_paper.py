6357946765c1d3a12e8f4910168fe7737112fd0b
Mikkel Kofoed <mikkel.kofoed@gmail.com>
2016-05-09 15:55:22 +0200

    New figures.

--- figures_for_paper.py
+++ figures_for_paper.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import settings as s
 import tools as t
-# import seaborn as sns
+from tqdm import tqdm
 
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
@@ -193,7 +193,7 @@ def D(name):
 # FIGURE 2 ---------------------------------------------------------------------------------------
 
 
-def subplot_fancyname(resolution=100):
+def Figure2(resolution=100):
     # For DE
 
     # Data for subplot 1
@@ -209,7 +209,7 @@ def subplot_fancyname(resolution=100):
         data_EU[i] = np.mean(G_B_EU) / np.mean(avg_L_EU)
 
     # Data for subplot 2
-    n = 21
+    n = 41
     alphas = np.linspace(0, 1.00, n)
     alpha = 1.00
 
@@ -245,81 +245,200 @@ def subplot_fancyname(resolution=100):
     fig1, (ax1) = plt.subplots(1, 1)
     fig2, (ax2) = plt.subplots(1, 1)
 
-    # Plot subplot 1
+    # Plot F21
     ax1.plot(alpha_list, data_DE, '-r', label=r'$\beta^T = 0$')
     ax1.plot(alpha_list, data_EU, '-b', label=r'$\beta^T = \infty$')
     ax1.set_ylabel(r'$\frac{\left< G^B_n \right>}{\left< L_n \right> }$',
-                   fontsize=15, rotation=0, labelpad=20)
+                   fontsize=20, rotation=0, labelpad=20)
     ax1.set_xlabel(r'$\alpha$', fontsize=20)
 
     # ax1.set_xlim([0, 1])
     # ax1.set_ylim([0, 0.6])
     ax1.legend(loc='lower left')
 
-    # Plot subplot 2
+    # Plot F22
     ax2.plot(alphas, K_B_n_b0_q9, ':r', label=r'$\beta^T=0, q=0.9$', alpha=0.75)
     ax2.plot(alphas, K_B_n_b0_q99, '-r', label=r'$\beta^T=0, q=0.99$', linewidth=2)
     ax2.plot(alphas, K_B_n_b0_q999, '--r', label=r'$\beta^T=0, q=0.999$', alpha=0.75)
     ax2.plot(alphas, K_B_n_binf_q9, ':b', label=r'$\beta^T=\infty, q=0.9$', alpha=0.75)
     ax2.plot(alphas, K_B_n_binf_q99, '-b', label=r'$\beta^T=\infty, q=0.99$', linewidth=2)
     ax2.plot(alphas, K_B_n_binf_q999, '--b', label=r'$\beta^T=\infty, q=0.999$', alpha=0.75)
-    ax2.legend(loc='lower left', fontsize=15)
+    ax2.legend(loc='lower left', fontsize=13)
     ax2.set_ylabel(r'$\frac{K_n^B \left(q\right)}{\left<L_n\right>}$',
                    rotation=0, fontsize=20, labelpad=20)
     ax2.set_xlabel(r'$\alpha$', fontsize=20)
+    # ax2.set_ylim([0, ])
 
     fig1.savefig(s.figures_folder + 'FIGURE2/' + 'F21.pdf')
     fig2.savefig(s.figures_folder + 'FIGURE2/' + 'F22.pdf')
     # plt.show()
     plt.close()
 
-subplot_fancyname()
-
 
 # FIGURE 4 ----------------------------------------------------------------------------------------
 
-def plot_figure4(summer_week=30, winter_week=60):
+def Figure4(summer_week=30, winter_week=60):
+    Gw_EU, Gs_EU, L_EU, avg_L_EU, D_n, C_n, G_B_n, D_EU, C_EU, G_B_EU = set_data(0.80, 1.0)
 
-    # Extracting data
-    Gw_EU, Gs_EU, L_EU, avg_L_EU, D_n, C_n, G_B_n, D_EU, C_EU, G_B_EU = set_data(0.8, 1.0)
+    N_b0 = np.load(s.nodes_fullname.format(c='c', f='s', a=0.80, b=0.00, g=1.00))
+    N_binf = np.load(s.nodes_fullname.format(c='c', f='s', a=0.80, b=np.inf, g=1.00))
 
-    G_B_DE = G_B_n[s.country_dict['DE']]
+    G_B_DE_b0 = N_b0.f.balancing[s.country_dict['DE']]
+    G_B_DE_binf = N_binf.f.balancing[s.country_dict['DE']]
+
     avg_L_DE = np.mean(L_EU[s.country_dict['DE']])
-    data = G_B_DE / avg_L_DE
+    data_b0 = G_B_DE_b0 / avg_L_DE / 1000
+    data_binf = G_B_DE_binf / avg_L_DE / 1000
 
     # Finding two weeks
 
-    summer_week = summer_week * 7 * 24
-    winter_week = winter_week * 7 * 24
+    summer_week *= 7 * 24
+    winter_week *= 7 * 24
     two_weeks = 24 * 7 * 2
-    data_summer = data[summer_week:summer_week + two_weeks]
-    data_winter = data[winter_week:winter_week + two_weeks]
+    data_summer_b0 = data_b0[summer_week:summer_week + two_weeks]
+    data_winter_b0 = data_b0[winter_week:winter_week + two_weeks]
+    data_summer_binf = data_binf[summer_week:summer_week + two_weeks]
+    data_winter_binf = data_binf[winter_week:winter_week + two_weeks]
 
-    x = [0, len(data_summer)]
+    x = [0, len(data_summer_b0)]
     x_ticks = ['$t$', '$t$ + 2 weeks']
-    # y = np.linspace(0, 1.25, 6)
 
-    fig, (ax) = plt.subplots(1, 1)
-    ax.plot(data_summer, 'y', label='Summer')
-    ax.plot(data_winter, 'b', label='Winter')
+    fig1, (ax1) = plt.subplots(1, 1)
+    ax1.plot(data_summer_b0, 'r', label=r'Summer $\beta^T=0$')
+    ax1.plot(data_summer_binf, 'b', label=r'Summer $\beta^T=\infty$')
 
-    # box = ax.get_position()
-    # ax.set_position([box.x0, box.y0, box.width * 0.5, box.height])
+    fig2, (ax2) = plt.subplots(1, 1)
 
-    ax.axhline(0.7, color='grey', alpha=0.75, linestyle='--', label=r'$\approx 0.7$')
-    ax.set_xticks(x)
-    # ax.set_yticks(y)
-    ax.set_xticklabels(x_ticks, ha="right", va="top", fontsize=20)
-    ax.set_xlim(x)
-    # ax.set_xlabel('$t$')
-    ax.set_ylabel(r'$\frac{G^B_n\left(t\right)}{\left<L_n\right>}$',
-                  rotation=0, fontsize=20, labelpad=20)
-    lgd = ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.savefig(s.figures_folder + 'FIGURE4/' + 'F4.pdf',
-                bbox_extra_artists=(lgd,), bbox_inches='tight')
+    ax2.plot(data_winter_b0, 'r', label=r'Winter $\beta^T=0$')
+    ax2.plot(data_winter_binf, 'b', label=r'Winter $\beta^T=\infty$')
+
+    ax1.axhline(0.7, color='grey', alpha=0.75, linestyle='--', label=r'$\approx 0.7$')
+    ax2.axhline(0.7, color='grey', alpha=0.75, linestyle='--', label=r'$\approx 0.7$')
+
+    ax1.set_xticks(x)
+    ax2.set_xticks(x)
+    ax1.set_xticklabels(x_ticks, ha="right", va="top", fontsize=20)
+    ax2.set_xticklabels(x_ticks, ha="right", va="top", fontsize=20)
+    ax1.set_xlim(x)
+    ax2.set_xlim(x)
+    ax1.set_ylim([0, 1.4])
+    ax2.set_ylim([0, 1.4])
+    ax1.set_ylabel(r'$\frac{G^B_n\left(t\right)}{\left<L_n\right>}$',
+                   rotation=0, fontsize=20, labelpad=20)
+    ax2.set_ylabel(r'$\frac{G^B_n\left(t\right)}{\left<L_n\right>}$',
+                   rotation=0, fontsize=20, labelpad=20)
+    ax1.legend(loc='upper right')
+    ax2.legend(loc='upper right')
+
+    fig1.savefig(s.figures_folder + 'FIGURE4/' + 'F41.pdf')
+    fig2.savefig(s.figures_folder + 'FIGURE4/' + 'F42.pdf')
+
     # plt.show()
     plt.close()
 
-# plot_figure4(summer_week=52*1.5, winter_week=52*2)
+
+def Figure5():
+    Gw_EU, Gs_EU, L_EU, avg_L_EU, D_n, C_n, G_B_n, D_EU, C_EU, G_B_EU = set_data(0.80, 1.0)
+
+    avg_L_DE = np.mean(L_EU[s.country_dict['DE']])
+
+    N_b0 = np.load(s.nodes_fullname.format(c='c', f='s', a=alpha, b=0.00, g=1.00))
+    N_binf = np.load(s.nodes_fullname.format(c='c', f='s', a=alpha, b=np.inf, g=1.00))
+
+    G_B_DE_b0 = N_b0.f.balancing[s.country_dict['DE']]
+    G_B_DE_binf = N_binf.f.balancing[s.country_dict['DE']]
+
+    G_B_DE_b0 /= (avg_L_DE * 1000)
+    G_B_DE_binf /= (avg_L_DE * 1000)
+
+    npoints = 151
+    stop = 1.5
+
+    avg_b0 = np.empty(npoints)
+    avg_binf = np.empty(npoints)
+
+    numbers_per_year_b0 = np.empty(npoints)
+    numbers_per_year_binf = np.empty(npoints)
+
+    x = np.linspace(0, stop, npoints)
+
+    for i, beta_B_n in enumerate(x):
+        K = beta_B_n
+        data_b0 = [G_B_n - K if G_B_n - K >= 0 else 0 for G_B_n in G_B_DE_b0]
+        data_binf = [G_B_n - K if G_B_n - K >= 0 else 0 for G_B_n in G_B_DE_binf]
+
+        numbers_per_year_b0[i] = np.count_nonzero(data_b0) / 8
+        numbers_per_year_binf[i] = np.count_nonzero(data_binf) / 8
+
+        avg_b0[i] = np.mean(data_b0)
+        avg_binf[i] = np.mean(data_binf)
+
+    fig1, (ax1) = plt.subplots(1, 1)
+    ax1.plot(x, avg_b0, 'r', label=r'$\beta^T=0$')
+    ax1.plot(x, avg_binf, 'b', label=r'$\beta^T=\infty$')
+    ax1.legend(loc='upper right')
+    ax1.set_xlabel(r'$K^B_n/\left<L_n\right>$')
+    ax1.set_ylabel(r'$\left< \max(G^B_n - K^B_n, 0) \right>$')
+    ax1.set_xlim([0, stop])
+
+    fig2, (ax2) = plt.subplots(1, 1)
+    ax2.plot(x, numbers_per_year_b0, 'r', label=r'$\beta^T=0$')
+    ax2.plot(x, numbers_per_year_binf, 'b', label=r'$\beta^T=\infty$')
+    ax2.legend(loc='upper right')
+    ax2.set_xlabel(r'$K^B_n/\left<L_n\right>$')
+    ax2.set_ylabel(r'$T(G^B_n > K^B_n)/yr$')
+
+    fig1.savefig(s.figures_folder + 'FIGURE5/' + 'F51.pdf')
+    fig2.savefig(s.figures_folder + 'FIGURE5/' + 'F52.pdf')
+
+    plt.close()
+
+
+def Figure6(dt=2*7*24):
+    # delta_t is in hours.
+    Gw_EU, Gs_EU, L_EU, avg_L_EU, D_n, C_n, G_B_n, D_EU, C_EU, G_B_EU = set_data(0.80, 1.0)
+
+    avg_L_DE = np.mean(L_EU[s.country_dict['DE']])
+
+    N_b0 = np.load(s.nodes_fullname.format(c='c', f='s', a=alpha, b=0.00, g=1.00))
+    N_binf = np.load(s.nodes_fullname.format(c='c', f='s', a=alpha, b=np.inf, g=1.00))
+
+    G_B_DE_b0 = N_b0.f.balancing[s.country_dict['DE']]
+    G_B_DE_binf = N_binf.f.balancing[s.country_dict['DE']]
+
+    G_B_DE_b0 /= (avg_L_DE * 1000)
+    G_B_DE_binf /= (avg_L_DE * 1000)
+
+    K_b0 = 0.1
+    K_binf = 0.5
+
+    G_minus_K_b0 = [G - K_b0 if G - K_b0 >= 0 else 0 for G in G_B_DE_b0]
+    G_minus_K_binf = [G - K_binf if G - K_binf >= 0 else 0 for G in G_B_DE_binf]
+
+    nonzero_b0 = np.nonzero(G_minus_K_b0)[0]
+    nonzero_binf = np.nonzero(G_minus_K_binf)[0]
+
+    N_qh_b0 = len(nonzero_b0)
+    N_qh_binf = len(nonzero_binf)
+
+    a = np.sum([np.sum(G_minus_K_b0[t:t+dt]) for t in nonzero_b0]) / N_qh_b0
+    print(a)
+
+    ## TODO:
+    #   Herfra skal jeg se, om 'a' er det rigtige. Skal så lave udregningen for K = linspace(0, 1, 5)
+    #   og B^T = 0 og inf og dt = 1=2*7*24
+
+
+
+
+    # fig1, (ax1) = plt.subplots(1, 1)
+    # ax1.plot(range(nhours), data)
+    # ax1.set_xlim([0, nhours])
+    # fig1.savefig(s.figures_folder + 'FIGURE6/' + 'F6.pdf')
+    # # plt.show()
+    # plt.close()
+
 # Figure1()
-subplot_fancyname()
+# Figure2()
+# Figure4(summer_week=26*7, winter_week=52+4)
+Figure6()
